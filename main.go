ca48801ecbbc442cd73d2d95cdeb28e3b5d4b339
douglas444 <douglas.m.cavalcanti@gmail.com>
2020-06-09 20:04:21 -0300

    add post struct

--- main.go
+++ main.go
@@ -1,17 +1,48 @@
 package main
 
 import (
+    "bufio"
+    "os"
     "fmt"
+	"strconv"
+	"strings"
     "github.com/douglas444/go-reddit-scraper/reddit"
 )
 
 func main() {
 
-    body, err := reddit.Search("teste");
+    reader := bufio.NewReader(os.Stdin);
+
+    fmt.Print("Enter query: ");
+    query, _ := reader.ReadString('\n');
+    query = strings.TrimSuffix(query, "\n");
+
+    fmt.Print("Enter sort [relevance, hot, top, new, comments]: ");
+    sort, _ := reader.ReadString('\n');
+    sort = strings.TrimSuffix(sort, "\n");
+
+    fmt.Print("Enter limit: ");
+    limitStr, _ := reader.ReadString('\n');
+    limitStr = strings.TrimSuffix(limitStr, "\n");
+    limit, err := strconv.Atoi(limitStr);
+
+    if err != nil {
+        fmt.Println(err);
+    }
+
+    posts, err := reddit.Search(query, sort, limit);
 
     if err != nil {
         fmt.Println(err);
     } else {
-        fmt.Println(body);
+        fmt.Println();
+        for _, post := range posts {
+            fmt.Printf("Upvotes: %d\n", post.Ups);
+            fmt.Printf("Downvotes: %d\n", post.Downs);
+            fmt.Printf("Comments number: %d\n", post.NumComments);
+            fmt.Printf("Media URL: %s\n", post.Url);
+            fmt.Printf("Subreddit: %s\n", post.Subreddit);
+            fmt.Printf("Title: %s\n\n", post.Title);
+        }
     }
 }
--- reddit/reddit.go
+++ reddit/reddit.go
@@ -1,33 +1,73 @@
 package reddit
 
 import (
+	"encoding/json"
     "fmt"
     "net/http"
-    "io/ioutil"
+    "net/url"
 )
 
-func Search(keyword string) (string, error) {
+type Post struct {
+    Id string `json:"id"`
+    Subreddit string `json:"subreddit"`
+    SelfText string `json:"selftext"`
+    Downs int `json:"downs"`
+    Ups int `json:"ups"`
+    NumComments int `json:"num_comments"`
+    ThumbnailWidth int `json:"thumbnail_width"`
+    ThumbnailHeight int `json:"thumbnail_height"`
+    Thumbnail string `json:"thumbnail"`
+    Title string `json:"title"`
+    Author string `json:"author"`
+    CreatedUTC float64 `json:"created_utc"`
+    Permalink string `json:"permalink"`
+    Url string `json:"url"`
+    IsVideo bool `json:"is_video"`
+}
+
+type postListing struct {
+	Kind string `json:"kind"`
+	Data struct {
+		Modhash  string `json:"modhash"`
+		Children []struct {
+			Kind string    `json:"kind"`
+			Data Post `json:"data"`
+		} `json:"children"`
+	} `json:"data"`
+}
+
+
+
+func Search(query string, sort string, limit int) ([]Post, error) {
 
     client := &http.Client{}
-    url := fmt.Sprintf("http://www.reddit.com/search.json?q=%s", keyword);
-    req, err := http.NewRequest("GET", url, nil)
+    urlStr := fmt.Sprintf("http://www.reddit.com/search.json?q=%s&sort=%s&limit=%d", url.QueryEscape(query), sort, limit);
+
+    req, err := http.NewRequest("GET", urlStr, nil)
     if err != nil {
-        return "", err;
+        return nil, err;
     }
 
     req.Header.Set("User-agent", "douglas444")
     resp, err := client.Do(req)
 
     if err != nil {
-        return "", err;
+        return nil, err;
     }
 
     defer resp.Body.Close();
 
-    body, err := ioutil.ReadAll(resp.Body);
-    if err != nil {
-        return "", err;
-    }
+	var result postListing;
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
 
-    return string(body), nil;
+	var posts []Post
+	for _, post := range result.Data.Children {
+		posts = append(posts, post.Data)
+	}
+
+	return posts, nil
+
 }
