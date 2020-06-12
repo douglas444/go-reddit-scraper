f0f3f9db931b49679b199c884c13d91443b20b5c
douglas444 <douglas.m.cavalcanti@gmail.com>
2020-06-12 16:41:07 -0300

    now workers will remove inactive jobs from channel

--- main.go
+++ main.go
@@ -44,19 +44,19 @@ func scrape(query string, sortBy string, windowSize int, lastId string) string {
     }
 }
 
-func worker(jobs chan Job) {
+func worker(jobs chan *Job) {
 
     for job := range jobs {
  
         if job.IsActive {
             job.LastId = scrape(job.Query, job.SortBy, job.WindowSize, job.LastId);
+            jobs <- job;
         }
 
-        jobs <- job;
     }
 }
 
-func serverStart(exit chan bool, jobById map[int]Job) {
+func serverStart(exit chan bool, jobById map[int]*Job, jobs chan *Job) {
     
     http.HandleFunc("/exit", func(w http.ResponseWriter, req *http.Request) {
         fmt.Println("[SERVER LOG] exiting...");
@@ -68,15 +68,14 @@ func serverStart(exit chan bool, jobById map[int]Job) {
         jobId, err := strconv.Atoi(strings.TrimPrefix(req.URL.Path, "/deactivate/"));
 
         if err != nil {
-		    w.WriteHeader(400);
+            w.WriteHeader(400);
             return;
         } else if _, isPresent := jobById[jobId]; !isPresent {
-		    w.WriteHeader(400);
-		    return;
+            w.WriteHeader(400);
+            return;
         } else {
-            job := jobById[jobId];
-            job.IsActive = false;
-            fmt.Println("[SERVER LOG] job", job.Id, "deactivated");
+            jobById[jobId].IsActive = false;
+            fmt.Println("[SERVER LOG] job", jobById[jobId].Id, "deactivated");
         }
     });
 
@@ -85,15 +84,15 @@ func serverStart(exit chan bool, jobById map[int]Job) {
         jobId, err := strconv.Atoi(strings.TrimPrefix(req.URL.Path, "/activate/"));
 
         if err != nil {
-		    w.WriteHeader(400);
-		    return;
-        } else if _, isPresent := jobById[jobId]; !isPresent {
-		    w.WriteHeader(400);
-		    return;
+            w.WriteHeader(400);
+            return;
+        } else if job, isPresent := jobById[jobId]; !isPresent || job.IsActive {
+            w.WriteHeader(400);
+            return;
         } else {
-            job := jobById[jobId];
-            job.IsActive = true;
-            fmt.Println("[SERVER LOG] job", job.Id, "activated");
+            jobById[jobId].IsActive = true;
+            jobs <- jobById[jobId];
+            fmt.Println("[SERVER LOG] job", jobById[jobId].Id, "activated");
         }
     });
 
@@ -106,23 +105,23 @@ func main() {
     queries := [3]string{"bolsonaro", "trump", "nicolás maduro"};
     workerPollSize := 2;
 
-    jobs := make(chan Job, len(queries) + 1);
+    jobs := make(chan *Job, len(queries) + 1);
 
     for i := 0; i < workerPollSize; i++ {
         go worker(jobs);
     }
 
-    jobById := make(map[int]Job);
+    jobById := make(map[int]*Job);
 
     for id, query := range queries {
         job := Job{id, query, 3, "new", "", true};
-        jobById[id] = job;
-        jobs <- job;
+        jobById[id] = &job;
+        jobs <- &job;
     }
 
     exit := make(chan bool);
 
-    go serverStart(exit, jobById);
+    go serverStart(exit, jobById, jobs);
 
     <- exit;
 
